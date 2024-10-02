61ec10007c15235b79f31aefb6e9fc5e74986ad0
Victor de Melo <victorgameplays535@gmail.com>
2024-10-02 10:47:27 -0300

    init: Inicio do Projeto / teste do victor

--- src/main/java/Teste.java
+++ src/main/java/Teste.java
@@ -2,5 +2,7 @@ public class Teste {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        System.out.println("Teste Victor commit");
     }
 }
