97f630bff8332e8931105b89859bc1e289082f5a
Isaque Fabro <isaquefabro04@gmail.com>
2024-11-04 17:45:38 -0200

    Get id adicionado em Amigos.java Alguns comentarios adicionados tambem

--- src/main/java/model/Amigos.java
+++ src/main/java/model/Amigos.java
@@ -1,17 +1,25 @@
 package model;
 
+
 public class Amigos {
 
-    private String nome;
-    private String telefone;
+    private int id; //Identificador do Amigo
+    private String nome; //Nome do Amigo
+    private String telefone; //Telefone do Amigo
+    //Operacao do banco de dados a fazer (Falta conectar netbeans com o respectivo banco de dados, e criar mais classes no package DAO)
 
     public Amigos() {
-        this("", "");
+        this(0, "", "");
     }
 
-    public Amigos(String nome, String telefone) {
+    public Amigos(int id, String nome, String telefone) {
+        this.id = id;
         this.nome = nome;
         this.telefone = telefone;
+    }
+    
+    public int getId(){
+        return id;
     }
 
     public String getNome() {
