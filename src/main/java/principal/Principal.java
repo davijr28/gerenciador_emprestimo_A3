42e584d19d8c47fe6e281dd5e92055811fbbf2fb
Isaque Fabro <isaquefabro04@gmail.com>
2024-11-12 04:23:51 -0200

    Criacao do pacote ConexaoDAO no package dao Insercao de codigo e imports na classe ConexaoDAO Insercao de dependencies no pom.xml Insercao do try catch na classe Principal, do package principal Tentativa de conexao com db

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -5,8 +5,23 @@ import view.MenuPrincipal;
 
 public class Principal {
     public static void main(String[] args) {
-        // Inicializa a janela principal do programa
+        // inicializar janela principal do programa
         MenuPrincipal janela = new MenuPrincipal();
         janela.setVisible(true);
         janela.setLocationRelativeTo(null);
+
+        // Tentativa de conexão com o db
+        try {
+            ConexaoDAO conexaoDAO = new ConexaoDAO();
+            if (conexaoDAO.getConnection() != null) {
+                System.out.println("Conectado com Sucesso");
+            } else {
+                System.out.println("Falha ao conectar: Retorno da conexão é nulo");
+            }
+        } catch (Exception e) {
+            // Diagnóstico de erro
+            System.out.println("Programa não conectado: " + e.getMessage());
+            //erros p diagnóstico
+        }
     }
+}
