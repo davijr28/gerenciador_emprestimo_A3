355eb9cc048d95265c0b65a05ffda28f28938482
Isaque Fabro <isaquefabro04@gmail.com>
2024-11-12 04:20:16 -0200

    Criacao do pacote ConexaoDAO no package dao Tentativa de conexao com db

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,12 +1,12 @@
 package principal;
 
-import view.CadastroAmigos;
+import dao.ConexaoDAO;
 import view.MenuPrincipal;
 
 public class Principal {
     public static void main(String[] args) {
+        // Inicializa a janela principal do programa
         MenuPrincipal janela = new MenuPrincipal();
         janela.setVisible(true);
         janela.setLocationRelativeTo(null);
     }
-}
