86aad5ad702a592459137bb0fdbe4e5bd026e34e
Isaque Fabro <isaquefabro04@gmail.com>
2024-11-12 13:15:43 -0200

    Database conectada com sucesso ConexaoDAO, Principal e pom.xml alterados

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -4,12 +4,8 @@ import dao.ConexaoDAO;
 import view.MenuPrincipal;
 
 public class Principal {
-    public static void main(String[] args) {
-        // inicializar janela principal do programa
-        MenuPrincipal janela = new MenuPrincipal();
-        janela.setVisible(true);
-        janela.setLocationRelativeTo(null);
 
+    public static void main(String[] args) {
         // Tentativa de conexão com o db
         try {
             ConexaoDAO conexaoDAO = new ConexaoDAO();
@@ -19,9 +15,9 @@ public class Principal {
                 System.out.println("Falha ao conectar: Retorno da conexão é nulo");
             }
         } catch (Exception e) {
-            // Diagnóstico de erro
+            // diagnóstico de erro
             System.out.println("Programa não conectado: " + e.getMessage());
-            //erros p diagnóstico
+            // detalhes do erro para diagnóstico
         }
     }
 }
