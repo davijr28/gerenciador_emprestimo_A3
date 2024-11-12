c97fb0dde9e4d06eae732efa2d05cd49a674b041
Isaque Fabro <isaquefabro04@gmail.com>
2024-11-12 13:23:09 -0200

    Inicializador da janela principal adicionado novamente

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -6,18 +6,23 @@ import view.MenuPrincipal;
 public class Principal {
 
     public static void main(String[] args) {
-        // Tentativa de conexão com o db
-        try {
-            ConexaoDAO conexaoDAO = new ConexaoDAO();
-            if (conexaoDAO.getConnection() != null) {
-                System.out.println("Conectado com Sucesso");
-            } else {
-                System.out.println("Falha ao conectar: Retorno da conexão é nulo");
-            }
-        } catch (Exception e) {
-            // diagnóstico de erro
-            System.out.println("Programa não conectado: " + e.getMessage());
-            // detalhes do erro para diagnóstico
+    // Inicializa a janela principal do programa
+    MenuPrincipal janela = new MenuPrincipal();
+    janela.setVisible(true);
+    janela.setLocationRelativeTo(null);
+}  
+    // Tentativa de conexão com o db
+    try static {
+        ConexaoDAO conexaoDAO = new ConexaoDAO();
+        if (conexaoDAO.getConnection() != null) {
+            System.out.println("Conectado com Sucesso");
+        } else {
+            System.out.println("Falha ao conectar: Retorno da conexão é nulo");
+        }
+    } catch (Exception e) {
+    // diagnóstico de erro
+        System.out.println("Programa não conectado: " + e.getMessage());
+        // detalhes do erro para diagnóstico
         }
     }
 }
