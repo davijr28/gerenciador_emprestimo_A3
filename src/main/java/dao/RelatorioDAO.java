0eed5fbc8559ca2d17b7c1677f2cbb9f5cd4341c
Isaque Fabro <isaquefabro04@gmail.com>
2024-11-13 21:14:02 -0200

    ArrayList adicionado na classe RelatorioDAO

--- src/main/java/dao/RelatorioDAO.java
+++ src/main/java/dao/RelatorioDAO.java
@@ -11,3 +11,15 @@ public class RelatorioDAO {
     private final ArrayList<RelatorioDAO> minhaLista = new ArrayList<>();
     private final ConexaoDAO db;
     
+// Construtor da classe RelatorioDAO.
+// Inicializa a conexão com o banco de dados.
+    public ArrayList<RelatorioDAO> getTotais() {
+        String query = "SELECT a.nome AS nome_amigo, f.nome AS nome_ferramenta, a.telefone AS telefone_amigo, f.preco AS preco " +
+                       "FROM tb_emprestimos e " +
+                       "JOIN tb_amigos a ON e.id_amigo = a.id " +
+                       "JOIN tb_ferramentas f ON e.id_ferramenta = f.id " +
+                       "ORDER BY nome_amigo;";
+        getRelatorio(query);
+        
+        return minhaLista;
+    }
