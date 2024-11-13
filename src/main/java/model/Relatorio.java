34c355d1503fa81895e896ea883c598e5ef834ec
Isaque Fabro <isaquefabro04@gmail.com>
2024-11-13 21:24:14 -0200

    Alguns bugs na classe Relatorio.java Tentando arrumar e deixar mais claro

--- src/main/java/model/Relatorio.java
+++ src/main/java/model/Relatorio.java
@@ -13,10 +13,41 @@ public class Relatorio {
     private Timestamp dataEmprestimo;
     private Date dataDevolucao;
     private boolean entregue;
+    private double preco;
 
     public Relatorio(String nomeAmigo, String nomeFerramenta, String telefoneAmigo, double preco) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
+// Getters e Setters para cada campo
+    public String getNomeAmigo() {
+        return nomeAmigo;
+    }
 
-   
+    public void setNomeAmigo(String nomeAmigo) {
+        this.nomeAmigo = nomeAmigo;
+    }
+
+    public String getNomeFerramenta() {
+        return nomeFerramenta;
+    }
+
+    public void setNomeFerramenta(String nomeFerramenta) {
+        this.nomeFerramenta = nomeFerramenta;
+    }
+
+    public String getTelefoneAmigo() {
+        return telefoneAmigo;
+    }
+    public void setTelefoneAmigo(String telefoneAmigo) {
+        this.telefoneAmigo = telefoneAmigo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
