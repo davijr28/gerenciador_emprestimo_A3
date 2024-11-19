9e089e17d9dfbbf69c1e1b9e9037fc6d07a80d85
davijr28 <davijr28@gmail.com>
2024-11-19 19:16:58 -0300

    fix: corrigir imports

--- src/main/java/dao/AmigoDAO.java
+++ src/main/java/dao/AmigoDAO.java
@@ -1,6 +1,11 @@
 package dao;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import model.Amigos;
 import java.util.ArrayList;
 
--- src/main/java/dao/FerramentaDAO.java
+++ src/main/java/dao/FerramentaDAO.java
@@ -1,6 +1,12 @@
 package dao;
 
-import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import model.Ferramentas;
 
